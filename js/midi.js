4d40d247e76a39fb839bef7807f421ac34799435
Chris Wilson <cwilso@gmail.com>
2012-09-04 17:33:09 -0700

    WebMIDI Polyfill port complete.

--- js/midi.js
+++ js/midi.js
@@ -3,20 +3,22 @@ var current_in;
 var msg;
 var sel;
 
-function midiProc(t,a,b,c) {
-  var cmd = a >> 4;
-  var channel = a & 0xf;
+function midiMessageReceived( msgs ) {
+  for (i=0; i<msgs.length; i++) {
+    var cmd = msgs[i].data[0] >> 4;
+    var channel = msgs[i].data[0] & 0xf;
+    var noteNumber = msgs[i].data[1];
+    var velocity = msgs[i].data[2];
 
-  var noteNumber = b;
-
-  if ( cmd==8 || ((cmd==9)&&(c==0)) ) { // with MIDI, note on with velocity zero is the same as note off
-    // note off
-    noteOff(b);
-  } else if (cmd == 9) {
-    // note on
-    noteOn(b, c/127);
-  } else if (cmd == 11) {
-    controller(b, c/127);
+    if ( cmd==8 || ((cmd==9)&&(velocity==0)) ) { // with MIDI, note on with velocity zero is the same as note off
+      // note off
+      noteOff( noteNumber );
+    } else if (cmd == 9) {
+      // note on
+      noteOn( noteNumber, velocity/127.0);
+    } else if (cmd == 11) {
+      controller( noteNumber, velocity/127.0);
+    }
   }
 }
 
@@ -52,8 +54,34 @@ function disconnectMidiIn(){
  catch(err){}
 }
 
+var selectMIDI = null;
+var midiAccess = null;
+var midiIn = null;
+
+function changeMIDIPort() {
+  var list=midiAccess.enumerateInputs();
+  midiIn = midi.getInput( list[ selectMIDI.selectedIndex ] );
+  midiIn.onmessage = midiMessageReceived;
+}
+
 function onMIDIStarted( midi ) {
+  midiAccess = midi;
+
   document.getElementById("synthbox").className = "loaded";
+
+  selectMIDI=document.getElementById("midiIn");
+  var list=midi.enumerateInputs();
+
+  // clear the MIDI input select
+  selectMIDI.options.length = 0;
+
+  if (list.length) {
+    for (var i=0; i<list.length; i++) {
+      selectMIDI.options[i]=new Option(list[i].name,list[i].fingerprint,i==0,i==0);
+    }
+    midiIn = midi.getInput( list[0] );
+    midiIn.onmessage = midiMessageReceived;
+  }
 }
 
 function onMIDISystemError( msg ) {
