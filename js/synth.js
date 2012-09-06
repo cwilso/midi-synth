84681ce306a0ee0fb29bc2887f5f6b258b0916b7
Chris Wilson <cwilso@gmail.com>
2012-09-06 14:00:37 -0700

    OSC1 + OSC2, filter, envelopes completely implemented.
    Still to do: modulation, drive, reverb

--- js/synth.js
+++ js/synth.js
@@ -1,16 +1,37 @@
 var voices = new Array();
 var audioContext = null;
-var FOURIER_SIZE = 4096;
-var wave = false;
-var ATTACKTIME = 0.1;
-var RELEASETIME = 0.5;
-var currentFilterFrequency = 2500;
-var currentFilterQ = 10;
-var currentOsc1Waveform = 0; // SINE
 
-var synthBox = null;
+var currentOsc1Waveform = 2; // SAW
+var currentOsc1Octave = 1;  // 16'
+var currentOsc1Detune = 0;	// 0
+var currentOsc1Mix = 50.0;	// 50%
+
+var currentOsc2Waveform = 2; // SAW
+var currentOsc2Octave = 1;  // 8'
+var currentOsc2Detune = 15;	// slight detune makes pretty analogue-y sound.  :)
+var currentOsc2Mix = 50.0;	// 0%
+
+var currentFilterFrequency = 1492.0;
+var currentFilterQ = 5.0;
+var currentFilterMod = 0;
+var currentFilterEnv = 67;
+
+var currentEnvA = 10;
+var currentEnvD = 15;
+var currentEnvS = 75;
+var currentEnvR = 20;
+
+var currentFilterEnvA = 35;
+var currentFilterEnvD = 15;
+var currentFilterEnvS = 50;
+var currentFilterEnvR = 40;
+
+var currentRev = 0;
+var currentOverdrive = 0;
+var currentVol = 50;
+
 var keys = new Array( 256 );
-keys[65] = 60; // = C4
+keys[65] = 60; // = C4 ("middle C")
 keys[87] = 61;
 keys[83] = 62;
 keys[69] = 63;
@@ -56,19 +77,22 @@ function noteOff( note ) {
 function controller( number, value ) {
   if (number == 1) {
     currentFilterFrequency = 5000 * value;
-    $("#fFreq input").val( currentFilterFrequency );
-    $("#fFreq input").trigger('change');
+    $("#fFreq").val( currentFilterFrequency );
+    $("#fFreq").trigger('change');
+    onUpdateFilterFrequency( currentFilterFrequency );
     return;
   } else if (number == 2) {
     currentFilterQ = 40 * value;
-    $("#fQ input").val( currentFilterFrequency );
-    $("#fQ input").trigger('change');
+    $("#fQ").val( currentFilterQ );
+    $("#fQ").trigger('change');
+    onUpdateFilterQ( currentFilterQ );
   }
 }
 
 function onUpdateFilterFrequency( value ) {
 	if (value.currentTarget)
 		value = value.currentTarget.value;
+	currentFilterFrequency = value;
 //	console.log("update Filter Freq: " + value);
 	for (var i=0; i<255; i++) {
 		if (voices[i] != null) {
@@ -80,12 +104,17 @@ function onUpdateFilterFrequency( value ) {
 function onUpdateFilterQ( value ) {
 	if (value.currentTarget)
 		value = value.currentTarget.value;
+	currentFilterQ = value;
 //	console.log("update Filter Q: " + value);
 	for (var i=0; i<255; i++) {
 		if (voices[i] != null) {
 			voices[i].setFilterQ( value );
 		}
 	}
+}
+
+function onUpdateFilterEnv( value ) {
+	currentFilterEnv = value;
 }
 
 function onUpdateOsc1Wave( ev ) {
@@ -97,7 +126,113 @@ function onUpdateOsc1Wave( ev ) {
 	}
 }
 
+function onUpdateOsc1Octave( ev ) {
+	currentOsc1Octave = ev.target.selectedIndex;
+	for (var i=0; i<255; i++) {
+		if (voices[i] != null) {
+			voices[i].updateOsc1Frequency();
+		}
+	}
+}
+
+function onUpdateOsc1Detune( value ) {
+	if (value.currentTarget)
+		value = value.currentTarget.value;
+	currentOsc1Detune = value;
+	for (var i=0; i<255; i++) {
+		if (voices[i] != null) {
+			voices[i].updateOsc1Frequency();
+		}
+	}
+}
+
+function onUpdateOsc1Mix( value ) {
+	if (value.currentTarget)
+		value = value.currentTarget.value;
+	currentOsc1Mix = value;
+	for (var i=0; i<255; i++) {
+		if (voices[i] != null) {
+			voices[i].updateOsc1Mix( value );
+		}
+	}
+}
+
+function onUpdateOsc2Wave( ev ) {
+	currentOsc2Waveform = ev.target.selectedIndex;
+	for (var i=0; i<255; i++) {
+		if (voices[i] != null) {
+			voices[i].setOsc2Waveform( currentOsc2Waveform );
+		}
+	}
+}
+
+function onUpdateOsc2Octave( ev ) {
+	currentOsc2Octave = ev.target.selectedIndex;
+	for (var i=0; i<255; i++) {
+		if (voices[i] != null) {
+			voices[i].updateOsc2Frequency();
+		}
+	}
+}
+
+function onUpdateOsc2Detune( value ) {
+	if (value.currentTarget)
+		value = value.currentTarget.value;
+	currentOsc2Detune = value;
+	for (var i=0; i<255; i++) {
+		if (voices[i] != null) {
+			voices[i].updateOsc2Frequency();
+		}
+	}
+}
+
+function onUpdateOsc2Mix( value ) {
+	if (value.currentTarget)
+		value = value.currentTarget.value;
+	currentOsc2Mix = value;
+	for (var i=0; i<255; i++) {
+		if (voices[i] != null) {
+			voices[i].updateOsc2Mix( value );
+		}
+	}
+}
+
+function onUpdateEnvA( value ) {
+	currentEnvA = value;
+}
+
+function onUpdateEnvD( value ) {
+	currentEnvD = value;
+}
+
+function onUpdateEnvS( value ) {
+	currentEnvS = value;
+}
+
+function onUpdateEnvR( value ) {
+	currentEnvR = value;
+}
+
+function onUpdateFilterEnvA( value ) {
+	currentFilterEnvA = value;
+}
+
+function onUpdateFilterEnvD( value ) {
+	currentFilterEnvD = value;
+}
+
+function onUpdateFilterEnvS( value ) {
+	currentFilterEnvS = value;
+}
+
+function onUpdateFilterEnvR( value ) {
+	currentFilterEnvR = value;
+}
+
 /*
+var FOURIER_SIZE = 4096;
+var wave = false;
+
 	if ( wave ) {
 		var real = new Float32Array(FOURIER_SIZE);
 		var imag = new Float32Array(FOURIER_SIZE);
@@ -115,44 +250,107 @@ function onUpdateOsc1Wave( ev ) {
 
 */
 function Voice( note, velocity ) {
-	this.osc = audioContext.createOscillator();
-	this.osc.frequency.value = frequencyFromNoteNumber( note );
-	this.osc.type = currentOsc1Waveform;
+	this.originalFrequency = frequencyFromNoteNumber( note );
+
+	// create osc 1
+	this.osc1 = audioContext.createOscillator();
+	this.updateOsc1Frequency();
+	this.osc1.type = currentOsc1Waveform;
+
+	this.osc1Gain = audioContext.createGainNode();
+	this.osc1Gain.gain.value = 0.0033 * currentOsc1Mix;
+//	this.gain.gain.value = 0.05 + (0.33 * velocity);
+	this.osc1.connect( this.osc1Gain );
+
+	// create osc 2
+	this.osc2 = audioContext.createOscillator();
+	this.updateOsc2Frequency();
+	this.osc2.type = currentOsc2Waveform;
+
+	this.osc2Gain = audioContext.createGainNode();
+	this.osc2Gain.gain.value = 0.0033 * currentOsc2Mix;
+	this.osc2.connect( this.osc2Gain );
+
+	this.filter1 = audioContext.createBiquadFilter();
+	this.filter1.type = this.filter1.LOWPASS;
+	this.filter1.Q.value = currentFilterQ;
+	this.filter2 = audioContext.createBiquadFilter();
+	this.filter2.type = this.filter2.LOWPASS;
+	this.filter2.Q.value = currentFilterQ;
+
+	this.osc1Gain.connect( this.filter1 );
+	this.osc2Gain.connect( this.filter1 );
+	this.filter1.connect( this.filter2 );
+
 	this.envelope = audioContext.createGainNode();
-	this.gain = audioContext.createGainNode();
-	this.gain.gain.value = 0.05 + (0.33 * velocity);
-	this.osc.connect( this.gain );
-	this.filter = audioContext.createBiquadFilter();
-	this.filter.type = this.filter.LOWPASS;
-	this.filter.frequency.value = currentFilterFrequency;
-	this.filter.Q.value = currentFilterQ;
-	this.gain.connect( this.filter );
-	this.filter.connect( this.envelope );
+	this.filter2.connect( this.envelope );
 	this.envelope.connect( audioContext.destination );
 	var now = audioContext.currentTime;
+	var envAttackEnd = now + (currentEnvA/100.0);
+	var filterAttackEnd = now + (currentFilterEnvA/100.0);
 	this.envelope.gain.setValueAtTime( 0, now );
-	this.envelope.gain.linearRampToValueAtTime( 1.0, now + ATTACKTIME );
-	this.osc.noteOn(0);
+	this.envelope.gain.linearRampToValueAtTime( 1.0, envAttackEnd );
+	this.envelope.gain.linearRampToValueAtTime( (currentEnvS/100.0), envAttackEnd + (currentEnvD/100.0) );
+	var initFilter = currentFilterFrequency * (1.0-(currentFilterEnv/100.0));
+	this.filter1.frequency.setValueAtTime( initFilter, now );
+	this.filter1.frequency.linearRampToValueAtTime( currentFilterFrequency, filterAttackEnd );
+	this.filter1.frequency.linearRampToValueAtTime( initFilter + (currentFilterFrequency * currentFilterEnv * currentFilterEnvS/10000.0), filterAttackEnd + (currentFilterEnvD/100.0) );
+	this.filter2.frequency.setValueAtTime( initFilter, now );
+	this.filter2.frequency.linearRampToValueAtTime( currentFilterFrequency, filterAttackEnd );
+	this.filter2.frequency.linearRampToValueAtTime( initFilter + (currentFilterFrequency * currentFilterEnv * currentFilterEnvS/10000.0), filterAttackEnd + (currentFilterEnvD/100.0) );
+	this.osc1.noteOn(0);
+	this.osc2.noteOn(0);
 }
 
 Voice.prototype.setOsc1Waveform = function( value ) {
-	this.osc.type = value;
+	this.osc1.type = value;
+}
+
+Voice.prototype.updateOsc1Frequency = function( value ) {
+	this.osc1.frequency.value = (this.originalFrequency*Math.pow(2,currentOsc1Octave-2));  // -2 because osc1 is 32', 16', 8'
+	this.osc1.detune.value = currentOsc1Detune;
+}
+
+Voice.prototype.updateOsc1Mix = function( value ) {
+	this.osc1Gain.gain.value = 0.0033 * value;
+}
+
+Voice.prototype.setOsc2Waveform = function( value ) {
+	this.osc2.type = value;
+}
+
+Voice.prototype.updateOsc2Frequency = function( value ) {
+	this.osc2.frequency.value = (this.originalFrequency*Math.pow(2,currentOsc2Octave-1));
+	this.osc2.detune.value = currentOsc2Detune;
+}
+
+Voice.prototype.updateOsc2Mix = function( value ) {
+	this.osc2Gain.gain.value = 0.0033 * value;
 }
 
 Voice.prototype.setFilterFrequency = function( value ) {
-	this.filter.frequency.value = value;
+	var now =  audioContext.currentTime;
+	this.filter1.frequency.cancelScheduledValues( now );
+	this.filter1.frequency.setValueAtTime(value, now );
+	this.filter2.frequency.cancelScheduledValues( now );
+	this.filter2.frequency.setValueAtTime(value, now );
+//	this.filter.frequency.value = value;
 }
 
 Voice.prototype.setFilterQ = function( value ) {
 //	this.filter.Q.setTargetValueAtTime( value, audioContext.currentTime, 0.1 );
-	this.filter.Q.value = value;
+	this.filter1.Q.value = value;
+	this.filter2.Q.value = value;
 }
 
 Voice.prototype.noteOff = function() {
 	var now =  audioContext.currentTime;
+	this.envelope.gain.cancelScheduledValues(now);
 	this.envelope.gain.setValueAtTime( this.envelope.gain.value, now );
-	this.envelope.gain.linearRampToValueAtTime( 0.0, now + RELEASETIME );
-	this.osc.noteOff( now + RELEASETIME );
+	var release = now + (currentEnvR/100.0);
+	this.envelope.gain.linearRampToValueAtTime( 0.0, release );
+	this.osc1.noteOff( release );
+	this.osc2.noteOff( release );
 }
 
 function keyDown( ev ) {
@@ -180,7 +378,6 @@ function initAudio() {
   	}
 	window.addEventListener('keydown', keyDown, false);
 	window.addEventListener('keyup', keyUp, false);
-	synthBox = document.getElementById("synthbox");
 	setupSynthUI();
 }
 
--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,5 @@
+var synthBox = null;
+
 function createKnob( id, label, width, x, y, min, max, currentValue, color, onChange ) {
 	var container = document.createElement( "div" );
 	container.className = "knobContainer";
@@ -8,6 +10,8 @@ function createKnob( id, label, width, x, y, min, max, currentValue, color, onCh
 	knob.className = "knob";
 	knob.id = id;
 	knob.value = currentValue;
+	if (label == "detune")
+		knob.setAttribute( "data-cursor", true );
 	knob.setAttribute( "data-min", min );
 	knob.setAttribute( "data-max", max );
 	knob.setAttribute( "data-width", width );
@@ -69,46 +73,61 @@ function createSection( label, x, y, width, height ) {
 }
 
 function setupSynthUI() {
+	synthBox = document.getElementById("synthbox");
+	
 	var mod = createSection( "mod", 10, 10, 87, 342 );
-	mod.style.borderColor = "#444";	
+
+	// mod is currently unimplemented
+	mod.style.borderColor = "#444";
+	mod.style.color = "gray";
+
 	mod.appendChild( createDropdown( "shape", 12, 15, ["sine","square", "saw", "triangle"], 0, null ))
-	mod.appendChild( createKnob( "mFreq", "freq", 80, 12, 65, 0, 20, 2, "#c10087", null ) );
-	mod.appendChild( createKnob( "mDepth1", "depth1", 80, 12, 160, 0, 100, 75, "#c10087", null ) );
-	mod.appendChild( createKnob( "mDepth2", "depth2", 80, 12, 255, 0, 100, 75, "#c10087", null ) );
+	mod.appendChild( createKnob( "mFreq", "freq*10", 80, 12, 65, 0, 200, 2, "#c10087", null ) );
+	mod.appendChild( createKnob( "mDepth1", "osc1 freq", 80, 12, 160, 0, 100, 75, "#c10087", null ) );
+	mod.appendChild( createKnob( "mDepth2", "osc2 freq", 80, 12, 255, 0, 100, 75, "#c10087", null ) );
 	synthBox.appendChild( mod );
 
 	var osc1 = createSection( "OSC1", 130, 10, 240, 160 );	
-	osc1.appendChild( createDropdown( "waveform", 10, 15, ["sine","square", "saw", "triangle", "wavetable"], 0, onUpdateOsc1Wave ))
-	osc1.appendChild( createDropdown( "interval", 160, 15, ["32'","16'", "8'"], 0, null ) );
-	osc1.appendChild( createKnob( "osc1detune", "detune", 100, 10, 65, -1200, 1200, 0, "blue", null ) );
-	osc1.appendChild( createKnob( "osc1mix", "mix", 100, 130, 65, 0, 100, 75, "blue", null ) );
+	osc1.appendChild( createDropdown( "waveform", 10, 15, ["sine","square", "saw", "triangle"/*, "wavetable"*/], currentOsc1Waveform, onUpdateOsc1Wave ))
+	osc1.appendChild( createDropdown( "interval",             160, 15, ["32'","16'", "8'"], currentOsc1Octave, onUpdateOsc1Octave ) );
+	osc1.appendChild( createKnob(     "osc1detune", "detune", 100, 10, 65, -1200, 1200, currentOsc1Detune, "blue", onUpdateOsc1Detune ) );
+	osc1.appendChild( createKnob(     "osc1mix", "mix",       100, 130, 65, 0, 100, currentOsc1Mix, "blue", onUpdateOsc1Mix ) );
 	synthBox.appendChild( osc1 );
 
 	var osc2 = createSection( "OSC2", 130, 192, 240, 160 );	
-	osc2.appendChild( createDropdown( "waveform", 10, 15, ["sine","square", "saw", "triangle", "wavetable"], 0, null ))
-	osc2.appendChild( createDropdown( "interval", 160, 15, ["16'","8'", "4'"], 0, null ) );
-	osc2.appendChild( createKnob( "osc2detune", "detune", 100, 10, 65, -500, 500, 0, "blue", null ) );
-	osc2.appendChild( createKnob( "osc2mix", "mix", 100, 130, 65, 0, 100, 0, "blue", null ) );
+	osc2.appendChild( createDropdown( "waveform", 10, 15, ["sine","square", "saw", "triangle"/*, "wavetable"*/], currentOsc2Waveform, onUpdateOsc2Wave ))
+	osc2.appendChild( createDropdown( "interval", 160, 15, ["16'","8'", "4'"], currentOsc2Octave, onUpdateOsc2Octave ) );
+	osc2.appendChild( createKnob( "osc2detune", "detune", 100, 10, 65, -1200, 1200, currentOsc2Detune, "blue", onUpdateOsc2Detune ) );
+	osc2.appendChild( createKnob( "osc2mix", "mix", 100, 130, 65, 0, 100, currentOsc2Mix, "blue", onUpdateOsc2Mix ) );
 	synthBox.appendChild( osc2 );
 
-	var filter = createSection( "filter", 404, 10, 100, 342 );	
-	filter.appendChild( createKnob( "fFreq", "freq", 85, 12, 65, 0, 5000, currentFilterFrequency, "#ffaa00", onUpdateFilterFrequency ) );
-	filter.appendChild( createKnob( "fQ", "q", 85, 12, 160, 0, 40, currentFilterQ, "#ffaa00", onUpdateFilterQ ) );
-	filter.appendChild( createKnob( "fMod", "mod", 85, 12, 255, 0, 100, 75, "#ffaa00", null ) );
+	var filter = createSection( "filter", 404, 10, 80, 342 );	
+	filter.appendChild( createKnob( "fFreq", "freq", 75, 12, 15, 0, 5000, currentFilterFrequency, "#ffaa00", onUpdateFilterFrequency ) );
+	filter.appendChild( createKnob( "fQ", "q",       75, 12, 100, 0, 20, currentFilterQ, "#ffaa00", onUpdateFilterQ ) );
+//	filter.appendChild( createKnob( "fMod", "mod",   75, 12, 185, 0, 100, currentFilterMod, "ffaa00", null ) );
+	filter.appendChild( createKnob( "fEnv", "env",   75, 12, 270, 0, 100, currentFilterEnv, "#ffaa00", onUpdateFilterEnv ) );
 	synthBox.appendChild( filter );
 
-	var filterEnv = createSection( "filter envelope", 538, 10, 355, 100 );	
-	filterEnv.appendChild( createKnob( "fA", "attack",  80,   10, 20, 0, 100, 50, "#bf8f30", null ) );
-	filterEnv.appendChild( createKnob( "fD", "decay",   80,  100, 20, 0, 100, 50, "#bf8f30", null ) );
-	filterEnv.appendChild( createKnob( "fS", "sustain", 80,  190, 20, 0, 100, 50, "#bf8f30", null ) );
-	filterEnv.appendChild( createKnob( "fR", "release", 80,  280, 20, 0, 100, 50, "#bf8f30", null ) );
+	var filterEnv = createSection( "filter envelope", 518, 10, 355, 98 );	
+	filterEnv.appendChild( createKnob( "fA", "attack",  80,   10, 20, 0, 100, currentFilterEnvA, "#bf8f30", onUpdateFilterEnvA ) );
+	filterEnv.appendChild( createKnob( "fD", "decay",   80,  100, 20, 0, 100, currentFilterEnvD, "#bf8f30", onUpdateFilterEnvD ) );
+	filterEnv.appendChild( createKnob( "fS", "sustain", 80,  190, 20, 0, 100, currentFilterEnvS, "#bf8f30", onUpdateFilterEnvS ) );
+	filterEnv.appendChild( createKnob( "fR", "release", 80,  280, 20, 0, 100, currentFilterEnvR, "#bf8f30", onUpdateFilterEnvR ) );
 	synthBox.appendChild( filterEnv );
 
-	var volumeEnv = createSection( "volume envelope", 538, 135, 355, 100 );	
-	volumeEnv.appendChild( createKnob( "vA", "attack",  80,   10, 20, 0, 100, 5, "#00b358", null ) );
-	volumeEnv.appendChild( createKnob( "vD", "decay",   80,  100, 20, 0, 100, 5, "#00b358", null ) );
-	volumeEnv.appendChild( createKnob( "vS", "sustain", 80,  190, 20, 0, 100, 75, "#00b358", null ) );
-	volumeEnv.appendChild( createKnob( "vR", "release", 80,  280, 20, 0, 100, 10, "#00b358", null ) );
+	var volumeEnv = createSection( "volume envelope", 518, 131, 355, 98 );	
+	volumeEnv.appendChild( createKnob( "vA", "attack",  80,   10, 20, 0, 100, currentEnvA, "#00b358", onUpdateEnvA ) );
+	volumeEnv.appendChild( createKnob( "vD", "decay",   80,  100, 20, 0, 100, currentEnvD, "#00b358", onUpdateEnvD ) );
+	volumeEnv.appendChild( createKnob( "vS", "sustain", 80,  190, 20, 0, 100, currentEnvS, "#00b358", onUpdateEnvS ) );
+	volumeEnv.appendChild( createKnob( "vR", "release", 80,  280, 20, 0, 100, currentEnvR, "#00b358", onUpdateEnvR ) );
 	synthBox.appendChild( volumeEnv );
+
+	var master = createSection( "master", 518, 254, 355, 98 );	
+	master.appendChild( createKnob( "ovr", "drive",      80,   10, 20, 0, 100, currentOverdrive, "yellow", null ) );
+	master.appendChild( createKnob( "rev", "reverb",     80,  100, 20, 0, 100, currentRev, "yellow", null ) );
+	master.appendChild( createKnob( "vol", "volume",     80,  190, 20, 0, 100, currentVol, "yellow", null ) );
+	master.appendChild( createDropdown( "midi_in", 280, 15, ["-none-","-", "-"], 0, null ) );
+	master.appendChild( createDropdown( "kbd_oct", 280, 60, ["-3", "-2","-1", "normal", "+1", "+2", "+3"], 3, null ) );
+	synthBox.appendChild( master );
 
 } 
