78cb47e493839b7dae8fd6d7fa96c968c7b9ba11
Chris Wilson <cwilso@gmail.com>
2014-05-21 15:17:36 -0700

    Two-octave keyboard layout

--- js/synth.js
+++ js/synth.js
@@ -39,6 +39,7 @@ var currentVol = 75;
 // end initial patch
 
 var keys = new Array( 256 );
+/* old mapping
 keys[65] = 60; // = C4 ("middle C")
 keys[87] = 61;
 keys[83] = 62;
@@ -59,6 +60,51 @@ keys[186] = 76;
 keys[222] = 77; // = F5
 keys[221] = 78;
 keys[13] = 79;
+keys[220] = 80;
+*/
+
+//Lower row: zsxdcvgbhnjm...
+keys[16] = 41; // = F2
+keys[65] = 42;
+keys[90] = 43;
+keys[83] = 44;
+keys[88] = 45;
+keys[68] = 46;
+keys[67] = 47;
+keys[86] = 48; // = C3
+keys[71] = 49;
+keys[66] = 50;
+keys[72] = 51;
+keys[78] = 52;
+keys[77] = 53; // = F3
+keys[75] = 54;
+keys[188] = 55;
+keys[76] = 56;
+keys[190] = 57;
+keys[186] = 58;
+keys[191] = 59;
+
+// Upper row: q2w3er5t6y7u...
+keys[81] = 60; // = C4 ("middle C")
+keys[50] = 61;
+keys[87] = 62;
+keys[51] = 63;
+keys[69] = 64;
+keys[82] = 65; // = F4
+keys[53] = 66;
+keys[84] = 67;
+keys[54] = 68;
+keys[89] = 69;
+keys[55] = 70;
+keys[85] = 71;
+keys[73] = 72; // = C5
+keys[57] = 73;
+keys[79] = 74;
+keys[48] = 75;
+keys[80] = 76;
+keys[219] = 77; // = F5
+keys[187] = 78;
+keys[221] = 79;
 keys[220] = 80;
 
 var effectChain = null;
@@ -604,7 +650,7 @@ function keyDown( ev ) {
 	var note = keys[ev.keyCode];
 	if (note)
 		noteOn( note + 12*(3-currentOctave), 0.75 );
-//	console.log( "key down: " + ev.keyCode );
+	console.log( "key down: " + ev.keyCode );
 
 	return false;
 }
